536562471e3fd23198712b8f47c3288b69e9460d
Goku <johnortega@bellsouth.net>
2019-07-04 00:31:28 -0400

    ticker to database

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,15 +2,22 @@ const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 // Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+// router.route("/")
+//   .get(booksController.findAll)
+//   .post(booksController.create);
 
 // Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+// router
+//   .route("/:id")
+//   .get(booksController.findById)
+//   .put(booksController.update)
+//   .delete(booksController.remove);
+
+  router
+  .route("/")
+  .post(booksController.savebook);
+  // .get(booksController.findById)
+  // .put(booksController.update)
+  // .delete(booksController.remove);
 
 module.exports = router;
