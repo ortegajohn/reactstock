3d2e6bf2c5a686868587f0db1a23aaa1ba3d164b
samlutz <sammlutz@gmail.com>
2019-07-15 20:15:35 -0400

    prepull

--- db/database_keys.js
+++ db/database_keys.js
@@ -5,7 +5,8 @@ module.exports = {
         host:       process.env.DB_HOST,
         user:       process.env.DB_USER,
         password:   process.env.DB_PASS,
-        database:   process.env.DB_NAME
+        database:   process.env.DB_NAME,
+        port:       process.env.DB_PORT
     }
 }
 
