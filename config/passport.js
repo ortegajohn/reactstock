4bae0d5e83e97d1a5807c687ab5b0b65115da303
Erik Johnson <ej3684@yahoo.com>
2019-07-16 21:10:10 -0400

    got password length, must fill in all fields completed

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const db = require('../models')
 const database = require('../db/database_connection');
 const {
     encryptPassword,
@@ -63,17 +63,72 @@ passport.use('local.signup', new LocalStrategy({
         password: password,
         first_name: firstname,
         last_name: lastname
+    } 
+
+    const check = async () =>{
+        if(!firstname || !lastname || !username || !password){
+            console.log({msg: 'Please enter all fields'});
+       } else {
+            if(password.length < 6) {
+              console.log({ msg: 'Password must be at least 6 characters'});
+             }else{
+                    database.query("SELECT * FROM iStock_users WHERE username = ?", [username]).then((req, res) => {
+                        //console.log('req: ', Object.keys(req));
+                        //console.log('res: ', req[0].username);
+                        console.log('req ', req.length);
+                        if(!req.lenght > 0){
+                            newUser.password = function operation(encryptPassword) {encryptPassword(password);}
+                                console.log('yo!:', newUser.password)
+                                database.query("INSERT INTO iStock_users SET ?", [newUser]).then(function(){
+                                    newUser.id = result.insertId;
+        
+                                    console.log(newUser);
+        
+                                    return done(null, newUser, req.flash('success', 'Welcome user: ', newUser.username));
+                                });
+                        } else {
+                            if(req[0].username) {
+                                console.log({ msg: 'User already exists'});
+                            } else {
+                                console.log('password: ', password);
+                                newUser.password = function operation(encryptPassword) {encryptPassword(password);}
+                                console.log('yo!:', newUser.password)
+                                database.query("INSERT INTO iStock_users SET ?", [newUser]).then(function(){
+                                    newUser.id = result.insertId;
+        
+                                    console.log(newUser);
+        
+                                    return done(null, newUser, req.flash('success', 'Welcome user: ', newUser.username));
+                                });
+        
+                                // newUser.id = result.insertId;
+        
+                                // console.log(newUser);
+        
+                                // return done(null, newUser, req.flash('success', 'Welcome user: ', newUser.username));
+                            } 
+
+                        }
+                            
+                            
+                        
+                    }) 
+          
+           
+            
+        }
     }
+    }
+        check();
+    // newUser.password = await encryptPassword(password);
 
-    newUser.password = await encryptPassword(password);
+    // const result = await database.query("INSERT INTO iStock_users SET ?", [newUser]);
 
-    const result = await database.query("INSERT INTO iStock_users SET ?", [newUser]);
+    // newUser.id = result.insertId;
 
-    newUser.id = result.insertId;
+    // console.log(newUser);
 
-    console.log(newUser);
-
-    return done(null, newUser, req.flash('success', 'Welcome user: ', newUser.username));
+    // return done(null, newUser, req.flash('success', 'Welcome user: ', newUser.username));
 
 }));
 
---
+++ models/User.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("iStock_users", {
+   first_name: DataTypes.STRING,
+   last_name: DataTypes.STRING,
+   username: DataTypes.STRING,
+   password: DataTypes.STRING
+  });
+  return User;
+};
