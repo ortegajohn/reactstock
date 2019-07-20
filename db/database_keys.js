0bef2ee406deb11fbc57f67e205e5d2ced2fcfab
John Ortega <43900756+ortegajohn@users.noreply.github.com>
2019-07-19 21:03:47 -0400

    Update database_keys.js

--- db/database_keys.js
+++ db/database_keys.js
@@ -6,7 +6,7 @@ module.exports = {
         user:       process.env.DB_USER,
         password:   process.env.DB_PASS,
         database:   process.env.DB_NAME,
-        port:       8889
+        port:       3306
     }
 }
 
